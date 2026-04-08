7f7e54b8e8458c98e9e97db661ced8abc1d42e89
zcyzcy98 <51557777+zcyzcy98@users.noreply.github.com>
2026-04-08 14:59:11 +0800

    fix: request URL

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://english-words-server.onrender.com/api",
   timeout: 100000,
 });
 
